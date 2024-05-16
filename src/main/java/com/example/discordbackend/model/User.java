7a56289ec4a39c5a0d79bb30dc0a15c36fe96bf6
hyehae <sdahye908@gmail.com>
2024-05-16 23:49:17 +0900

    create model folder

---
+++ src/main/java/com/example/discordbackend/model/User.java
@@ -0,0 +1,38 @@
+package com.example.discordbackend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userId;
+
+    @Column(nullable = false, length = 50)
+    private String loginId;
+
+    @Column(nullable = false, length = 100)
+    private String password;
+
+    @Column(nullable = false, length = 50)
+    private String email;
+
+    @Column(nullable = false, length = 30)
+    private String userName;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+}
